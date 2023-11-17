5b1dbd223627bd13d2866cf79a8c97c051ad0f94
Blake Vieyra <blake.vieyra@gmail.com>
2023-11-17 15:25:40 -0800

    before project update

--- src/CashRegister.java
+++ src/CashRegister.java
@@ -1,9 +1,14 @@
+import java.util.Scanner;
 
 public class CashRegister {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		System.out.println("Please enter the amount ");
+		System.out.println("Please enter the amount ");
+		System.out.println("Please enter the amount ");
 
+		
 	}
 
 }
